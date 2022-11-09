4ef42396584f29b5fab451203694fe6dd4fa8abe
Netetra <netetra071507@gmail.com>
2022-11-09 12:46:43 +0900

    2022/11/09/12:46

--- .install.sh
+++
@@ -1,8 +0,0 @@
-# Install Sheldon
-cargo install sheldon
-sheldon init --shell zsh
-eval "$(sheldon source)"
-
-# Install Alacritty
-cargo install alacritty
-git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
---
+++ install.sh
@@ -0,0 +1,30 @@
+# Rust Setup
+echo "Installing Rust stable..."
+rustup default stable > /dev/null
+echo "Installing Rust tools... "
+rustup component add clippy rls rust-analysis rust-src rust-docs rustfmt > /dev/null
+
+# Install Sheldon
+echo "Installing sheldon..."
+cargo install sheldon > /dev/null
+sheldon init --shell zsh
+eval "$(sheldon source)"
+
+# Install Alacritty
+cargo install alacritty
+git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
+
+# Python Setup
+
+
+# Clone dotfiles
+mkdir ~/git
+mkdir ~/git/dotfiles
+git clone https://github.com/Netetra/dotfiles.git ~/git/dotfiles
+
+#Create SymbolicLink
+ln -s ~/git/dotfiles/.zshrc ~/.zshrc
+ln -s ~/git/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/git/dotfiles/.config ~/.config
+ln -s ~/git/dotfiles/.sheldon ~/.sheldon
+ln -s ~/git/dotfiles/.background ~/.background
