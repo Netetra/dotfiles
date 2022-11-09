820be198a1c728c35c406ae2d04c4aedb4d6ae6d
Netetra <netetra071507@gmail.com>
2022-11-10 00:14:55 +0900

    2022/11/10/0:14

--- install.sh
+++ install.sh
@@ -2,30 +2,10 @@
 mkdir ~/git
 cd git
 git clone https://github.com/Netetra/dotfiles.git
-git clone https://github.com/romkatv/powerlevel10k.git powerlevel10k
 cd ~
 
 #Create SymbolicLink
 ln -s ~/git/dotfiles/.zshrc ~/.zshrc
 ln -s ~/git/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/git/dotfiles/.config ~/.config
-ln -s ~/git/dotfiles/.sheldon ~/.sheldon
 ln -s ~/git/dotfiles/.background ~/.background
-
-# Rust Setup
-echo "Installing Rust stable..."
-rustup default stable > /dev/null
-echo "Installing Rust tools... "
-rustup component add clippy rls rust-analysis rust-src rust-docs rustfmt > /dev/null
-
-# Install Sheldon
-echo "Installing sheldon..."
-sheldon init --shell zsh
-
-# Install Alacritty
-echo "Installing Alacritty..."
-cargo install alacritty
-mkdir ~/.alacritty-colorscheme
-git clone https://github.com/eendroroy/alacritty-theme.git ~/.alacritty-colorscheme
-
-
