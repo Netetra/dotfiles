b6823ed03d1bdbe357a22bf694c9efdda1aa9aed
Netetra <netetra071507@gmail.com>
2022-11-10 16:24:53 +0900

    2022/11/10/16:24

--- install.sh
+++ install.sh
@@ -13,17 +13,3 @@ ln -sf /home/$user_name/git/dotfiles/.config/waybar /home/$user_name/.config/way
 ln -sf /home/$user_name/git/dotfiles/.config/alacritty /home/$user_name/.config/alacritty
 ln -sf /home/$user_name/git/dotfiles/.config/wofi /home/$user_name/.config/wofi
 ln -sf /home/$user_name/git/dotfiles/.background /home/$user_name/.background
-
-# Rust Setup
-#echo "Installing Rust stable..."
-#sudo -u $user_name bash -c "cd /home/$user_name && rustup default stable > /dev/null"
-
-#echo "Installing Rust tools... "
-#sudo -u $user_name bash -c "cd /home/$user_name && rustup component add clippy rls rust-analysis rust-src rust-docs rustfmt > /dev/null"
-
-# Install Sheldon
-#echo "Installing sheldon..."
-#sudo -u $user_name bash -c "cd /home/$user_name && sheldon init --shell zsh"
-#sudo -u $user_name bash -c "cd /home/$user_name && sheldon add zsh-autosuggestions --github zsh-users/zsh-autosuggestions"
-#sudo -u $user_name bash -c "cd /home/$user_name && sheldon add zsh-completions --github zsh-users/zsh-completions"
-#sudo -u $user_name bash -c "cd /home/$user_name && sheldon add zsh-syntax-highlighting --github zsh-users/zsh-syntax-highlighting"
