d3f54de17a3213a9e31a5a7e3c5feff99c76317a
Netetra <netetra071507@gmail.com>
2022-11-19 23:11:28 +0900

    2022/11/19/23:11

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ git clone https://github.com/Netetra/dotfiles.git
 cd /home/$user_name
 
 #Create SymbolicLink
-mkdir /home/$user_name/.config
-ln -sf /home/$user_name/git/dotfiles/.zshrc /home/$user_name/.zshrc
-ln -sf /home/$user_name/git/dotfiles/.tmux.conf /home/$user_name/.tmux.conf
-ln -sf /home/$user_name/git/dotfiles/.config/sway /home/$user_name/.config/sway
-ln -sf /home/$user_name/git/dotfiles/.config/waybar /home/$user_name/.config/waybar
-ln -sf /home/$user_name/git/dotfiles/.config/alacritty /home/$user_name/.config/alacritty
-ln -sf /home/$user_name/git/dotfiles/.config/wofi /home/$user_name/.config/wofi
-ln -sf /home/$user_name/git/dotfiles/.background /home/$user_name/.background
+mkdir ~/.config
+ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/git/dotfiles/.config/sway ~/.config/sway
+ln -sf ~/git/dotfiles/.config/waybar ~/.config/waybar
+ln -sf ~/git/dotfiles/.config/alacritty ~/.config/alacritty
+ln -sf ~/git/dotfiles/.config/wofi ~/.config/wofi
+ln -sf ~/git/dotfiles/.background ~/.background
+ln -sf ~/git/dotfiles/.config/nvim ~/.config/nvim
